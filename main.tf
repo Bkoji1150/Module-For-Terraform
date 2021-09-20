6c5f64c9294cdb764523e56c09c5fe59121f380e
Bkoji1150 <kojibello058@gmail.com>
2021-09-20 19:15:41 -0400

    making use of terraform module hosted in github

--- main.tf
+++ main.tf
@@ -16,36 +16,6 @@ module "networking" {
 }
 
 
-//resource "aws_vpc" "kj-vpc" {
-//  cidr_block           = var.cidr_block
-//  instance_tenancy     = "default"
-//  enable_dns_support   = var.enable_dns
-//  enable_dns_hostnames = var.enable_hostname
-//
-//  tags = {
-//    Name = lookup(var.tagname, terraform.workspace)
-//  }
-//}
-//resource "aws_internet_gateway" "kj-IGW" {
-//  vpc_id = module.networking.vpc-koji
-//
-//  tags = {
-//    Name = "kj-IGW"
-//  }
-//
-//}
-//
-//resource "aws_subnet" "kj-pub-subnet" {
-//  vpc_id                  = module.networking.vpc-koji
-//  cidr_block              = var.gen-subnet.dev[1]
-//  availability_zone       = var.gen-az.dev[1]
-//  map_public_ip_on_launch = true
-//
-//  tags = {
-//    Name = "kj-pub-subnet"
-//  }
-//}
-
 resource "aws_subnet" "kj-pri-subnet" {
   vpc_id            = module.networking.vpc-koji
   cidr_block        = var.gen-subnet.dev[0]
