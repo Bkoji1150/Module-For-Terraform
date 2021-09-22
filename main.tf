17ad17438fd991ee0db51166219d9ee674dfdf31
Bkoji1150 <kojibello058@gmail.com>
2021-09-22 18:54:04 -0400

    made and update to the main.tf

--- main.tf
+++ main.tf
@@ -8,11 +8,9 @@ terraform {
 module "networking" {
   source            = "git@github.com:Bkoji1150/module-for-terraform.git//networkmodule"
   cidr_block        = var.cidr_block
-  enable_dns        = var.enable_dns
+  enable_dns        = false
   enable_hostname   = var.enable_hostname
   tagname           = lookup(var.tagname, terraform.workspace)
   cidr_block_public = var.gen-subnet.dev[1]
   availability_zone = var.gen-az.dev[1]
-  cidr_block_pri    = var.gen-subnet.dev[0]
-  availability_pri  = var.gen-az.dev[0]
 }
